4fd944d71108611f1a8df4a85eed8ccc9398c676
Gustavo Cardona <admin@gustavocardona.com>
2026-04-15 09:20:20 -0700

    diseños acoplados a imagen y marca

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,36 @@
 const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.dot');
 let index = 0;
+let autoTimer;
 
 function showSlide(i) {
-  slides.forEach(slide => slide.classList.remove('active'));
+  slides.forEach(s => s.classList.remove('active'));
+  dots.forEach(d => d.classList.remove('active'));
   slides[i].classList.add('active');
+  dots[i].classList.add('active');
+}
+
+function goToSlide(i) {
+  index = i;
+  showSlide(index);
+  resetTimer();
+}
+
+function changeSlide(dir) {
+  index = (index + dir + slides.length) % slides.length;
+  showSlide(index);
+  resetTimer();
 }
 
 function nextSlide() {
-  index++;
-  if (index >= slides.length) index = 0;
+  index = (index + 1) % slides.length;
   showSlide(index);
 }
 
+function resetTimer() {
+  clearInterval(autoTimer);
+  autoTimer = setInterval(nextSlide, 5000);
+}
+
 // Auto play cada 5s
-setInterval(nextSlide, 5000);
+autoTimer = setInterval(nextSlide, 5000);
